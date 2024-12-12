56a07c8ac9b0e23a9bda2dcfcfd3aba294e2d99f
Jacopo Malvaso <jacopo.malvaso@cern.ch>
2024-12-12 14:56:55 +0100

    Ad hoc Weight applied on Z->ee

--- httcp/weights/main.py
+++ httcp/weights/main.py
@@ -23,6 +23,13 @@ def main(self: WeightProducer, events: ak.Array, **kwargs) -> ak.Array:
     weight = ak.Array(np.ones(len(events), dtype=np.float32))
     for column in self.weight_columns:
         weight = weight * Route(column).apply(events)
+    processes = self.dataset_inst.processes.names()
+    process_id = events.process_id
+    if ak.any(['dy' in proc for proc in processes]) :
+        print("Applying an ad hoc weight to Z->ee...")
+        weight = ak.where(process_id==51001,weight*1.3,weight)
+        print("The ad hoc weight to Z->ee was applied succefully...")
+        
     return events, weight
 
 
